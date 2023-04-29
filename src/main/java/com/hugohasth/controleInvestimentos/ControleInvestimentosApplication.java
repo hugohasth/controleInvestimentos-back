75d35fe98d58c081d154125006d65403d829bb97
hugohasth <hugohasth@gmail.com>
2023-04-29 11:15:47 -0300

    Commit inicial

---
+++ src/main/java/com/hugohasth/controleInvestimentos/ControleInvestimentosApplication.java
@@ -0,0 +1,13 @@
+package com.hugohasth.controleInvestimentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleInvestimentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleInvestimentosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hugohasth/controleInvestimentos/ControleInvestimentosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hugohasth.controleInvestimentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleInvestimentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
