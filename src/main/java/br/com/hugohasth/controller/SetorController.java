c32b3c9148c94bcb488d55126c249a668edba36b
hugohasth <hugohasth@gmail.com>
2023-05-17 15:39:43 -0300

    criado método para buscar setor por id

--- src/main/java/br/com/hugohasth/controller/SetorController.java
+++ src/main/java/br/com/hugohasth/controller/SetorController.java
@@ -3,7 +3,9 @@ package br.com.hugohasth.controller;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,6 +27,13 @@ public class SetorController {
 	@GetMapping
 	public List<Setor> list() {
 		return setorRepository.findAll();
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Setor> findById(@PathVariable Long id) {
+		return setorRepository.findById(id)
+				.map(setor -> ResponseEntity.ok().body(setor))
+				.orElse(ResponseEntity.notFound().build());
 	}
 	
 	@PostMapping
