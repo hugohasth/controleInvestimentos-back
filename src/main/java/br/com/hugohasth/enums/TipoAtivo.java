4f2f671b8f6d745b04efa8f0fcf04d6db93644af
hugohasth <hugohasth@gmail.com>
2024-07-07 12:15:57 -0300

    Refatoração para usar Enums no objeto Ativo. Seria bom revisitar isso depois para decidir se vamos deixar como Enum ou se vamos criar tabelas e entidades para guardar esse domínio.

---
+++ src/main/java/br/com/hugohasth/enums/TipoAtivo.java
@@ -0,0 +1,13 @@
+package br.com.hugohasth.enums;
+
+public enum TipoAtivo {
+    ACAO,
+    FUNDO_IMOBILIARIO,
+    TESOURO_DIRETO,
+    CDB,
+    LCI,
+    LCA,
+    DEBENTURE,
+    ETF,
+    CRIPTOMOEDA
+}
